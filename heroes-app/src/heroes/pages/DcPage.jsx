7696781310f40f9d397226f021f4d03bacafcd30
Ernesto Cardenas <ernesto.cardenas@unosqueare.com>
2025-06-12 09:53:50 -0600

    cargando lista de heroes para marvel

--- heroes-app/src/heroes/pages/DcPage.jsx
+++ heroes-app/src/heroes/pages/DcPage.jsx
@@ -1,11 +1,10 @@
 import { HeroesList } from "../components"
 
 export const DCPage = ()=>{
-    return ( <>
+    return ( 
+      <>
         <h1>DC Page</h1>      
-        {/* <HeroesContainer asociacion="DC Comics" /> */}
         <HeroesList publisher="DC Comics"></HeroesList>
-
       </>
     )
 }

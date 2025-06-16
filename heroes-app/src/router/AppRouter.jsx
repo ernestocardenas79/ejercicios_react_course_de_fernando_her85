bd7d6b63a6b28729a2260925e0e3e87e874e3bba
Ernesto Cardenas <ernesto.cardenas@unosqueare.com>
2025-06-16 17:41:45 -0600

    feat: ep216 | Rutas publicas y privadas | agregar rutas públicas y privadas | implementar lógica de enrutamiento para manejar acceso según estado de autenticación

--- heroes-app/src/router/AppRouter.jsx
+++ heroes-app/src/router/AppRouter.jsx
@@ -2,13 +2,30 @@ import { Route, Routes } from 'react-router-dom';
 
 import { LoginPage } from '../auth';
 import { HeroesRoutes } from '../heroes/';
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoutes } from './PublicRoutes';
 
 export const AppRouter = () => {
     return (
         <>
             <Routes>
-                <Route path="login" element={<LoginPage />}></Route>
-                <Route path="/*" element={<HeroesRoutes />}></Route>
+                <Route
+                    path="login"
+                    element={
+                        <PublicRoutes>
+                            <LoginPage></LoginPage>
+                        </PublicRoutes>
+                    }
+                />
+
+                <Route
+                    path="/*"
+                    element={
+                        <PrivateRoute>
+                            <HeroesRoutes />
+                        </PrivateRoute>
+                    }
+                />
             </Routes>
         </>
     );

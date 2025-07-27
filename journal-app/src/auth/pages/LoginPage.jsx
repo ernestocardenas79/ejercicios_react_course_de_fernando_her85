86fe3ca0a79625cfae782d9f2277323a3dce58e9
Ernesto Cardenas <ernesto.cardenas79@gmail.com>
2025-07-26 21:33:58 -0600

    feat: rediseñar página de inicio de sesión con MUI y mejorar estilos

--- journal-app/src/auth/pages/LoginPage.jsx
+++ journal-app/src/auth/pages/LoginPage.jsx
@@ -1,3 +1,33 @@
+import { Grid, TextField, Typography } from "@mui/material";
+
 export const LoginPage = () => {
-    return <h1>Login Page</h1>;
+    return (
+        <Grid
+            container
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
+            spacing={0}
+            sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }}
+        >
+            <Grid item
+            className='box-shadow'
+            xs={3}
+            sx={{backgroundColor:'white', padding:3, borderRadius:2}}
+            >
+                <Typography variant="h5" sx={{ mb: 1}}>Login</Typography>
+
+                <form>
+                    <Grid container>
+                        <Grid item xs={12} sx={{ mt: 2 }}>
+                            <TextField type="email" placeholder="correo@goo.com" label="Correo"/>
+                        </Grid>
+                        <Grid item xs={12} sx={{ mt: 2 }}>
+                            <TextField type="password" placeholder="Contraseña" label="Contraseña"/>
+                        </Grid>
+                    </Grid>
+                </form>
+            </Grid>
+        </Grid>
+    )
 };
